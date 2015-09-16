1d5877f5ed821c09909ee756971fa89064485083
袁栩栩 <yxx@1001hao.com>
2015-09-16 16:09:27 +0800

    更新phpmyadmin 版本

--- projects/phpmyadmin/libraries/rte/rte_export.lib.php
+++ projects/phpmyadmin/libraries/rte/rte_export.lib.php
@@ -13,21 +13,24 @@ if (! defined('PHPMYADMIN')) {
  * This function is called from one of the other functions in this file
  * and it completes the handling of the export functionality.
  *
- * @param  string  $item_name    The name of the item that we are exporting
- * @param  string  $export_data  The SQL query to create the requested item
+ * @param string $export_data The SQL query to create the requested item
+ *
+ * @return void
  */
-function PMA_RTE_handleExport($item_name, $export_data)
+function PMA_RTE_handleExport($export_data)
 {
     global $db;
 
-    $item_name = htmlspecialchars(PMA_backquote($_GET['item_name']));
+    $item_name = htmlspecialchars(PMA_Util::backquote($_GET['item_name']));
     if ($export_data !== false) {
         $export_data = '<textarea cols="40" rows="15" style="width: 100%;">'
                      . htmlspecialchars(trim($export_data)) . '</textarea>';
         $title = sprintf(PMA_RTE_getWord('export'), $item_name);
         if ($GLOBALS['is_ajax_request'] == true) {
-            $extra_data = array('title' => $title);
-            PMA_ajaxResponse($export_data, true, $extra_data);
+            $response = PMA_Response::getInstance();
+            $response->addJSON('message', $export_data);
+            $response->addJSON('title', $title);
+            exit;
         } else {
             echo "<fieldset>\n"
                . "<legend>$title</legend>\n"
@@ -35,12 +38,15 @@ function PMA_RTE_handleExport($item_name, $export_data)
                . "</fieldset>\n";
         }
     } else {
-        $_db = htmlspecialchars(PMA_backquote($db));
-        $response = __('Error in Processing Request') . ' : '
+        $_db = htmlspecialchars(PMA_Util::backquote($db));
+        $message  = __('Error in processing request:') . ' '
                   . sprintf(PMA_RTE_getWord('not_found'), $item_name, $_db);
-        $response = PMA_message::error($response);
+        $response = PMA_message::error($message);
         if ($GLOBALS['is_ajax_request'] == true) {
-            PMA_ajaxResponse($response, false);
+            $response = PMA_Response::getInstance();
+            $response->isSuccess(false);
+            $response->addJSON('message', $message);
+            exit;
         } else {
             $response->display();
         }
@@ -50,6 +56,8 @@ function PMA_RTE_handleExport($item_name, $export_data)
 /**
  * If necessary, prepares event information and passes
  * it to PMA_RTE_handleExport() for the actual export.
+ *
+ * @return void
  */
 function PMA_EVN_handleExport()
 {
@@ -57,14 +65,16 @@ function PMA_EVN_handleExport()
 
     if (! empty($_GET['export_item']) && ! empty($_GET['item_name'])) {
         $item_name = $_GET['item_name'];
-        $export_data = PMA_DBI_get_definition($db, 'EVENT', $item_name);
-        PMA_RTE_handleExport($item_name, $export_data);
+        $export_data = $GLOBALS['dbi']->getDefinition($db, 'EVENT', $item_name);
+        PMA_RTE_handleExport($export_data);
     }
 } // end PMA_EVN_handleExport()
 
 /**
  * If necessary, prepares routine information and passes
  * it to PMA_RTE_handleExport() for the actual export.
+ *
+ * @return void
  */
 function PMA_RTN_handleExport()
 {
@@ -75,11 +85,12 @@ function PMA_RTN_handleExport()
         && ! empty($_GET['item_type'])
     ) {
         if ($_GET['item_type'] == 'FUNCTION' || $_GET['item_type'] == 'PROCEDURE') {
-            $export_data = PMA_DBI_get_definition(
+            $export_data = $GLOBALS['dbi']->getDefinition(
                 $db,
                 $_GET['item_type'],
-                $_GET['item_name']);
-            PMA_RTE_handleExport($_GET['item_name'], $export_data);
+                $_GET['item_name']
+            );
+            PMA_RTE_handleExport($export_data);
         }
     }
 } // end PMA_RTN_handleExport()
@@ -87,6 +98,8 @@ function PMA_RTN_handleExport()
 /**
  * If necessary, prepares trigger information and passes
  * it to PMA_RTE_handleExport() for the actual export.
+ *
+ * @return void
  */
 function PMA_TRI_handleExport()
 {
@@ -94,7 +107,7 @@ function PMA_TRI_handleExport()
 
     if (! empty($_GET['export_item']) && ! empty($_GET['item_name'])) {
         $item_name = $_GET['item_name'];
-        $triggers = PMA_DBI_get_triggers($db, $table, '');
+        $triggers = $GLOBALS['dbi']->getTriggers($db, $table, '');
         $export_data = false;
         foreach ($triggers as $trigger) {
             if ($trigger['name'] === $item_name) {
@@ -102,7 +115,7 @@ function PMA_TRI_handleExport()
                 break;
             }
         }
-        PMA_RTE_handleExport($item_name, $export_data);
+        PMA_RTE_handleExport($export_data);
     }
 } // end PMA_TRI_handleExport()
 ?>
